86a19a89cd1f188e874d3f787e459e668438e054
JCorr <jccorr@calpoly.edu>
2017-04-09 16:00:39 -0700

    Add files via upload

---
+++ Teacher.java
@@ -0,0 +1,11 @@
+public class Teacher {
+  String tLastName, tFirstName;
+  int classroom;
+ 
+  public Teacher(String tLastName, String tFirstName, int classroom) {
+    super();
+    this.tLastName = tLastName;
+    this.tFirstName = tFirstName;
+    this.classroom = classroom;
+  }
+}
